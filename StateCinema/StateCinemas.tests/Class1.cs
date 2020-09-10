0259eb10d587e4bf5a01f444e87ee99cc45e58fb
muaz-aldairi <59302571+muaz-aldairi@users.noreply.github.com>
2020-09-11 11:58:30 +1200

    Add files via upload
    Adding files

---
+++ StateCinema/StateCinemas.tests/Class1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace StateCinemas
+{
+    [TestFixture]
+    public class StateCinematests
+    {
+        [TestCase]
+        public void Adult_Before_5_test()
+        {
+            // Arrange
+            StateCinemas sut = new StateCinemas();
+            // Act 
+            decimal lastPrice = sut.Adult_Before_5(3, "Adult", "Monday", 4);
+            // Assert
+            Assert.AreEqual(lastPrice, 43.5);
+        }
+
+
+        [TestCase]
+        public void Adult_After_5_test()
+        {
+            // Arrange
+            StateCinemas sut = new StateCinemas();
+            // Act 
+            decimal lastPrice = sut.Adult_After_5(2, "Adult", "Monday", 6);
+            // Assert
+            Assert.AreEqual(lastPrice,35);
+        }
+
+        [TestCase]
+        public void Adult_Tuesday_test()
+        {
+            // Arrange
+            StateCinemas sut = new StateCinemas();
+            // Act 
+            decimal lastPrice = sut.Adult_Tuesday(2, "Adult", "Tuesday");
+            // Assert
+            Assert.AreEqual(lastPrice, 26);
+        }
+
+
+        [TestCase]
+        public void Child_Under_16_test()
+        {
+            // Arrange
+            StateCinemas sut = new StateCinemas();
+            // Act 
+            decimal lastPrice = sut.Child_Under_16(3,"Child");
+            // Assert
+            Assert.AreEqual(lastPrice, 36);
+        }
+
+
+        [TestCase]
+        public void Senior_test()
+        {
+            // Arrange
+            StateCinemas sut = new StateCinemas();
+            // Act 
+            decimal lastPrice = sut.Senior(2, "Senior");
+            // Assert
+            Assert.AreEqual(lastPrice, 25);
+        }
+
+        [TestCase]
+        public void Student_test()
+        {
+            // Arrange
+            StateCinemas sut = new StateCinemas();
+            // Act 
+            decimal lastPrice = sut.Student(2,"Student");
+            // Assert
+            Assert.AreEqual(lastPrice, 28);
+        }
+
+    }
+
+}
+
+
+
---
+++ StateCinema/StateCinemas/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateCinemas
+{
+    public class StateCinemas
+    {
+       public Decimal Adult_Before_5 (int quantity, string person, string day, decimal time) 
+        {
+            decimal totalPrice = 0;
+           decimal ticketPrice = 14.50M;
+            if( person=="Adult" && time<5 && day!="Tuesday") {
+
+                totalPrice = quantity * ticketPrice;
+            }
+
+            return totalPrice;
+        }
+
+
+        public Decimal Adult_After_5(int quantity, string person, string day, decimal time)
+        {
+            decimal totalPrice = 0;
+            decimal ticketPrice = 17.50M;
+            if (person == "Adult" && time >= 5 && day != "Tuesday")
+            {
+
+                totalPrice = quantity * ticketPrice;
+            }
+
+            return totalPrice;
+        }
+
+
+        public Decimal Adult_Tuesday(int quantity, string person, string day)
+        {
+            decimal totalPrice = 0;
+            decimal ticketPrice = 13M;
+            if (person == "Adult" && day == "Tuesday")
+            {
+
+                totalPrice = quantity * ticketPrice;
+            }
+
+            return totalPrice;
+        }
+
+        public Decimal Child_Under_16(int quantity, string person)
+        {
+            decimal totalPrice = 0;
+            decimal ticketPrice = 12M;
+            if (person == "Child")
+            {
+
+                totalPrice = quantity * ticketPrice;
+            }
+
+            return totalPrice;
+        }
+
+        public Decimal Senior(int quantity, string person)
+        {
+            decimal totalPrice = 0;
+            decimal ticketPrice = 12.5M;
+            if (person == "Senior")
+            {
+
+                totalPrice = quantity * ticketPrice;
+            }
+
+            return totalPrice;
+        }
+
+        public Decimal Student(int quantity, string person)
+        {
+            decimal totalPrice = 0;
+            decimal ticketPrice = 14M;
+            if (person == "Student")
+            {
+
+                totalPrice = quantity * ticketPrice;
+            }
+
+            return totalPrice;
+        }
+    }
+}
